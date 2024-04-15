90d683ff568eead104a5d9307b6b2610ec1c3eab
Jingcheng Wang <jingwang49@gmail.com>
2024-04-15 20:00:31 +0800

    components setup

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import { Header } from './Header'
+import { Balance } from './Balance'
+import { IncomeExpense } from './IncomeExpense'
+import { TransactionList } from './TransactionList'
+import { AddTransaction } from './AddTransaction'
+
+export {
+    Header,
+    Balance,
+    IncomeExpense,
+    TransactionList,
+    AddTransaction,
+}
