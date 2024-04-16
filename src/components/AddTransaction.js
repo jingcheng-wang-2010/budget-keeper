499e9a618a0eba975792b7b07c9463baa40ff8e5
Jingcheng Wang <jingwang49@gmail.com>
2024-04-16 11:20:39 +0800

    delete and add transaction complete, form clears after submitting

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,13 +1,34 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+import { GlobalContext } from '../context/GlobalState'
 
 export const AddTransaction = () => {
     const [text, setText] = useState('')
     const [amount, setAmount] = useState(0)
 
+    const { addTransaction } = useContext(GlobalContext)
+
+    const uuid = uuidv4()
+
+    const onSubmit = e => {
+        e.preventDefault()
+
+        const newTransaction = {
+            //id: Math.floor(Math.random() * 10000000),
+            id: uuid,
+            text,
+            amount: +amount
+        }
+
+        addTransaction(newTransaction)
+        setText('')
+        setAmount(0)
+    }
+
     return (
         <>
             <h3>Add new transaction</h3>
-            <form>
+            <form onSubmit={onSubmit}>
                 <div className="form-control">
                     <label htmlFor="text">Text</label>
                     <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." />
--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,5 +1,18 @@
 export default (state, action) => {
+    console.log(state)
+
     switch(action.type) {
+        case 'DELETE_TRANSACTION':
+            return {
+                ...state,
+                transactions: state.transactions.filter(transaction => transaction.id !== action.payload)
+            }
+        case 'ADD_TRANSACTION':
+            return {
+                ...state,
+                transactions: [action.payload, ...state.transactions]
+            }
+                    
         default:
             return state
     }
